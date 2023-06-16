f5c80b64fc5c81bc6fb363a42294281f75760445
arberlisaj <arberlisaj1@gmail.com>
2023-06-16 20:32:30 +0200

    add: footer section

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
       <div className="header-wrapper">
         <header>
           <div className="logo-container">
-          <img src={Illustration} alt="website logo" />
+            <img src={Illustration} alt="website logo" />
             <h1>Vite & Github</h1>
           </div>
           <p>
@@ -40,22 +40,68 @@ const App = () => {
               deploy.yml . The result should look like this
               .github/workflows/deploy.yml
             </li>
-            <li> Inside deploy.yml use this code:{' '}
-             <a target="_blank" href="https://github.com/arberLisaj/deploy-vite-app/blob/main/.github/workflows/deploy.yml">Click here for the github code</a> 
+            <li>
+              {" "}
+              Inside deploy.yml use this code:{" "}
+              <a
+                target="_blank"
+                href="https://github.com/arberLisaj/deploy-vite-app/blob/main/.github/workflows/deploy.yml"
+              >
+                Click here for the github code
+              </a>
             </li>
-            <li>The last step is adding the repo name to vite.config.js like the <a target="blank" href="https://github.com/arberLisaj/deploy-vite-app/blob/main/vite.config.js">example here</a></li>
+            <li>
+              The last step is adding the repo name to vite.config.js like the{" "}
+              <a
+                target="blank"
+                href="https://github.com/arberLisaj/deploy-vite-app/blob/main/vite.config.js"
+              >
+                example here
+              </a>
+            </li>
             <li>Push the changes to github</li>
           </ol>
 
-          <h3>And we are set configuring the files next we need to give permission to github actions doing the following steps:</h3>
+          <h3>
+            And we are set configuring the files next we need to give permission
+            to github actions doing the following steps:
+          </h3>
           <ol>
-            <li>Go to the repository that you want to deploy for example : <b>arberlisaj/deploy-vite-app</b></li>
-            <li>Then <b> settings/Actions/General/Workflow </b> permissions Read and write permission should be  <b>checked</b> . Save and then go back to your repository</li>
-            <li>There should be some failed jobs so you want to <b>Re-Run all jobs </b> and the build and deployment will be completed succesfully</li>
-            <li>To Deploy your repo / app with github pages go to <b>settings/Pages/Branch </b> select hg-pages and the folder should be /(root)</li>
-            <li>Click Save and your project is now hosted with github pages refresh the page and your page link will appear</li>
+            <li>
+              Go to the repository that you want to deploy for example :{" "}
+              <b>arberlisaj/deploy-vite-app</b>
+            </li>
+            <li>
+              Then <b> settings/Actions/General/Workflow </b> permissions Read
+              and write permission should be <b>checked</b> . Save and then go
+              back to your repository
+            </li>
+            <li>
+              There should be some failed jobs so you want to{" "}
+              <b>Re-Run all jobs </b> and the build and deployment will be
+              completed succesfully
+            </li>
+            <li>
+              To Deploy your repo / app with github pages go to{" "}
+              <b>settings/Pages/Branch </b> select hg-pages and the folder
+              should be /(root)
+            </li>
+            <li>
+              Click Save and your project is now hosted with github pages
+              refresh the page and your page link will appear
+            </li>
           </ol>
         </section>
+
+        <footer>
+          <ul>
+            <li>facebook</li>
+            <li>twitter</li>
+            <li>github</li>
+            <li>instagram</li>
+          </ul>
+          <p> &copy; Arberlisaj | All rights reserved</p>
+        </footer>
       </main>
     </>
   );
